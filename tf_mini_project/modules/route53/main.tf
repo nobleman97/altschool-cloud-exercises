db0e07404fa25a1af90a0f0a4b4e8e3733d3d78f
nobleman97 <davidstone097@gmail.com>
2023-02-05 12:09:18 +0100

    change route53 type to AAAA

--- tf_mini_project/modules/route53/main.tf
+++ tf_mini_project/modules/route53/main.tf
@@ -5,7 +5,7 @@
 resource "aws_route53_record" "subdomain" {
   zone_id = aws_route53_zone.my_domain.zone_id
   name    = "terraform-test.${var.domain_name}"
-  type    = "A"
+  type    = "AAAA"
 
   alias {
     name                   = var.elb_dns_name
